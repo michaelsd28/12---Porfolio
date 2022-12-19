ddd71cd91b34ea6175dc169f6eb32211d243cb3c
Michael Severino <michael11281@hotmail.com>
2022-12-19 04:23:30 -0400

    build folder added

--- porfolio-app/src/components/home/Landing.jsx
+++ porfolio-app/src/components/home/Landing.jsx
@@ -185,6 +185,7 @@ const Landing = () => {
               ]}
               loop={Infinity}
               wrapper="h1"
+              
             />
           </Grid>
           <Grid        item xs={12} sm={12} md={12} lg={12} xl={12}>
